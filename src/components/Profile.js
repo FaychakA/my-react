5512a4fc830005ed3cc5dfaace98a042f10cc21c
Andrey Faychak <faychak333@gmail.com>
2019-08-15 17:56:27 +0300

    my first save with git

---
+++ src/components/Profile.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+    return (<div className='content'>
+        <div>
+            <img src='https://klike.net/uploads/posts/2019-01/1547365376_1.jpg' />
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            My posps
+            <div>
+                new post
+            </div>
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div>
+    </div>)
+}
+
+export default Profile;
